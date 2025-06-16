47d9f01b887de6c44a73d2b2470ccaa92d9f421e
Munde Immaculate <immaculatemunde1@gmail.com>
2025-06-16 11:05:34 +0300

    Update feedbackDiv with Registration is successful

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
         feedbackDiv.style.display = "block";
 
         if (isValid) {
-            feedbackDiv.textContent = "Registration is successful!";
+            feedbackDiv.textContent = "Registration successful!";
             feedbackDiv.style.color = "#28a745";
             feedbackDiv.style.backgroundColor = "#d4edda";
         } else {
